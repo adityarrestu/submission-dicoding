e5b5c08f9c647b575f91abc7d93e6c27a17562d0
Aditya Restu Adji <adit.restu77@gmail.com>
2022-09-19 14:36:38 +0700

    btn menu & nav transition

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,13 @@
 const nav = document.getElementById('nav')
 const content = document.getElementById('content')
+const aside = document.getElementById('aside')
 const menuToggle = document.getElementById('toggle')
 const root = document.documentElement
 
 const btnDropdown = document.getElementById('btn-dropdown')
 const kategori = document.getElementById('kategori')
+
+const btnMenu = document.getElementById('btn-menu')
 
 menuToggle.addEventListener('click', () => {
   nav.classList.toggle('close')
@@ -15,4 +18,10 @@ menuToggle.addEventListener('click', () => {
 kategori.addEventListener('click', () => {
   kategori.classList.toggle('open')
   btnDropdown.classList.toggle('open')
+})
+
+btnMenu.addEventListener('click', () => {
+  nav.classList.toggle('open')
+  content.classList.toggle('open-menu')
+  aside.classList.toggle('open-menu')
 })
