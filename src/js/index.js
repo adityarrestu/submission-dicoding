e19ed1625915130dbcd430408d760501355f1e6a
Aditya Restu Adji <adit.restu77@gmail.com>
2022-09-19 08:21:01 +0700

    tarnsition

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,16 @@ const content = document.getElementById('content')
 const menuToggle = document.getElementById('toggle')
 const root = document.documentElement
 
+const btnDropdown = document.getElementById('btn-dropdown')
+const kategori = document.getElementById('kategori')
+
 menuToggle.addEventListener('click', () => {
   nav.classList.toggle('close')
   content.classList.toggle('menu-close')
   menuToggle.classList.toggle('close')
+})
+
+kategori.addEventListener('click', () => {
+  kategori.classList.toggle('open')
+  btnDropdown.classList.toggle('open')
 })
